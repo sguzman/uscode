204280216b056d801b596306be2caa9e226c101f
Salvador Guzman <guzmansalv@gmail.com>
2022-02-18 20:45:21 -0800

    fix print

--- table3/ParseTable.py
+++ table3/ParseTable.py
@@ -12,30 +12,29 @@ from bs4 import BeautifulSoup
 import re
 import sys
 
+
 def parseTable(f):
-   
+
     soup = BeautifulSoup(f)
     f.close()
-    
+
     t = soup.find('table')
     t3 = t.find_all('tr')
 
-
     sections = []
     for row in t3:
-        cols = map(lambda x: x.get_text(),row.find_all("td"))
+        cols = map(lambda x: x.get_text(), row.find_all("td"))
         if len(cols) > 4:
-            sections.append((cols[2],cols[3]))
+            sections.append((cols[2], cols[3]))
     return sections
-                
-                
+
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 2:
-        print 'Usage: python ParseTable.py inputHTMLfile.htm'
+        print('Usage: python ParseTable.py inputHTMLfile.htm')
         sys.exit()
     filename = sys.argv[1]
-    f = file(filename,'r')
+    f = file(filename, 'r')
     sections = parseTable(f)
-    print sections
+    print(sections)
